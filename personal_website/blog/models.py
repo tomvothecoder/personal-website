e0377542aa46704d150386d19c441c76fe05ef4a
Tom Vo <t_vo16@u.pacific.edu>
2019-07-28 11:58:40 -0700

    Update blog models

--- personal_website/blog/models.py
+++ personal_website/blog/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -12,4 +15,8 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
-    image = models.FilePathField(path="/img")
+    image = models.ImageField(upload_to='post_images', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.created_on} - {self.categories.name}'
+
