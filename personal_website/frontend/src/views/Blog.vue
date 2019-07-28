2e13acd1c937e9d2157d52a10988a5790ed4a61c
Tom Vo <t_vo16@u.pacific.edu>
2019-07-28 14:12:18 -0700

    Add skeleton Blog view and update routers
    - Closes #8
    - Closes #11

--- personal_website/frontend/src/views/Blog.vue
+++ personal_website/frontend/src/views/Blog.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="blog">
+        <h1>This is the blog page</h1>
+        {{posts}}
+    </div>
 </template>
+
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "blog",
+        data() {
+            return {
+                posts: null
+            }
+        },
+        mounted: function () {
+            this.FetchData()
+        },
+        methods: {
+            FetchData: function () {
+                var app = this;
+                axios
+                    .get('http://localhost:8000/api/v1/blog/posts/')
+                    .then(response => (app.posts = response.data))
+            }
+        }
+    };
+
+</script>
