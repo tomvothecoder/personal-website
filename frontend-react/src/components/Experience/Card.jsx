0a01c89893bcb54a5b8b07c25e4e920b9f038c6d
tomvothecoder <tomvothecoder@gmail.com>
2020-04-18 13:31:12 -0700

    Add Card component - Add prop-types package

--- frontend-react/src/components/Experience/Card.jsx
+++ frontend-react/src/components/Experience/Card.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Card() {
-  return <div></div>;
+function Card({ imgSrc, href, length, company, jobTitle, location, children }) {
+  Card.propTypes = {
+    imgSrc: PropTypes.elementType.isRequired,
+    href: PropTypes.string.isRequired,
+    company: PropTypes.string.isRequired,
+    length: PropTypes.string.isRequired,
+    jobTitle: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+  };
+
+  return (
+    <div className="card">
+      <div className="card-content">
+        <div className="columns">
+          <div className="column is-one-quarter">
+            <figure className="image is-3by3">
+              <img src={imgSrc} alt={company} />
+            </figure>
+          </div>
+          <div className="column is-three-quarters">
+            <a
+              href={href}
+              className="company"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {company}
+            </a>
+            <p className="job-title">
+              {jobTitle}, <span className="is-italic">{length}</span>
+            </p>
+            <p className="job-location has-text-weight-bold">{location}</p>
+            <p className="job-desc">{children}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- frontend-react/src/components/Experience/index.jsx
+++ frontend-react/src/components/Experience/index.jsx
@@ -1,122 +1,101 @@
 import React from 'react';
 
+import Card from './Card';
+
 import energeiaLogo from '../../assets/energeia.png';
+import intelLogo from '../../assets/intel.png';
+import llnlLogo from '../../assets/llnl.png';
 import uopTower from '../../assets/uop_tower.jpg';
 
 function Experience() {
   return (
     <div id="experience">
-      <div class="columns is-centered" data-aos="fade-up" data-aos-delay="75">
-        <div class="column is-three-quarters">
-          <div class="card">
-            <div class="card-content">
-              <div class="columns">
-                <div class="column is-one-quarter">
-                  <figure class="image is-3by3">
-                    <img src={energeiaLogo} alt="Energeia USA" />
-                  </figure>
-                </div>
-                <div class="column is-three-quarters">
-                  <a
-                    href="http://energeia-usa.com/"
-                    class="company"
-                    target="_blank"
-                  >
-                    Energeia USA
-                  </a>
-                  <p class="job-title">
-                    Software Developer,
-                    <span class="is-italic">2018 - Present</span>
-                  </p>
-                  <p class="job-location has-text-weight-bold">Davis, CA</p>
-                  <p class="job-desc">
-                    At Energeia, I was the lead software engineer in charge of
-                    transitioning uSim,the world's most advanced Python energy
-                    simulation software, onto a SaaS platform. I also performed
-                    data analytics on modeling data sets to develop dashboards,
-                    which aided the work of the consulting team. It was an
-                    awesome being apart a face-paced start-up environment
-                    performing cutting-edge research for the energy industry.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div
+        className="columns is-centered"
+        data-aos="fade-up"
+        data-aos-delay="75"
+      >
+        <div className="column is-three-quarters">
+          <Card
+            imgSrc={llnlLogo}
+            href="https://www.llnl.gov/"
+            jobTitle="Software Developer"
+            company="Lawrence Livermore National Lab"
+            length="2020 - Present"
+            location="Livermore, CA"
+          ></Card>
         </div>
       </div>
-      <div class="columns is-centered" data-aos="fade-up" data-aos-delay="75">
-        <div class="column is-three-quarters">
-          <div class="card">
-            <div class="card-content">
-              <div class="columns">
-                <div class="column is-one-quarter">
-                  <figure class="image is-3by2" id="intel-logo">
-                    <img
-                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Intel-logo.svg/1200px-Intel-logo.svg.png"
-                      alt="Intel"
-                    />
-                  </figure>
-                </div>
-                <div class="column is-three-quarters">
-                  <a href="https://intel.com" class="company" target="_blank">
-                    Intel Corporation
-                  </a>
-                  <p class="job-title">
-                    Technical Marketing Engineer Intern,
-                    <span class="is-italic">2017 - 2018</span>
-                  </p>
-                  <p class="job-location has-text-weight-bold">Folsom, CA</p>
-                  <p class="job-desc">
-                    Interning at Intel was an amazing experience where I flexed
-                    my engineering muscles within the business world. I aided
-                    internal research on market segments by implenting Python
-                    scripts to automate the flow of data collection. My research
-                    lead to key decisions in product roadmap planning for
-                    Embedded SSDs. In the process, I learned foundational skills
-                    such managing projects and understanding the role of
-                    stakeholders in within those projects.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div
+        className="columns is-centered"
+        data-aos="fade-up"
+        data-aos-delay="75"
+      >
+        <div className="column is-three-quarters">
+          <Card
+            imgSrc={energeiaLogo}
+            href="http://energeia-usa.com"
+            jobTitle="Software Developer"
+            company="Energeia USA"
+            length="2018 - 2020"
+            location="Davis, CA"
+          >
+            At Energeia, I was the lead software engineer in charge of
+            transitioning uSim,the world&apos;s most advanced Python energy
+            simulation software, onto a SaaS platform. I also performed data
+            analytics on modeling data sets to develop dashboards, which aided
+            the work of the consulting team. It was an awesome being apart a
+            face-paced start-up environment performing cutting-edge research for
+            the energy industry.
+          </Card>
         </div>
       </div>
-      <div class="columns is-centered" data-aos="fade-up" data-aos-delay="75">
-        <div class="column is-three-quarters">
-          <div class="card">
-            <div class="card-content">
-              <div class="columns">
-                <div class="column is-one-quarter">
-                  <figure class="image is-3by3">
-                    <img src={uopTower} alt="Energeia USA" />
-                  </figure>
-                </div>
-                <div class="column is-three-quarters">
-                  <a href="https://pacific.edu" class="company" target="_blank">
-                    University of the Pacific
-                  </a>
-                  <p class="job-title">
-                    B.S. in Computer Science,
-                    <span class="is-italic">earned May 2018</span>
-                  </p>
-                  <p class="job-location has-text-weight-bold">Stockton, CA</p>
-                  <p></p>
-                  <p class="job-desc">
-                    UOP's awesome engineering and computer science program built
-                    the foundation of how to be a professional engineer. They
-                    push their students to seek real world experience through
-                    co-ops, which are extended internships that are baked into
-                    the undergrad degree program. I was deeply engaged in
-                    community service through Alpha Phi Omega, an international
-                    service organization. My experience at UOP helped shape
-                    critical life skills, including empathy, understanding,
-                    communication, and leadership.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div
+        className="columns is-centered"
+        data-aos="fade-up"
+        data-aos-delay="75"
+      >
+        <div className="column is-three-quarters">
+          <Card
+            imgSrc={intelLogo}
+            href="https://intel.com"
+            jobTitle="Technical Marketing Engineering Intern"
+            company="Intel"
+            length="2017 - 2018"
+            location="Folsom, CA"
+          >
+            Interning at Intel was an amazing experience where I flexed my
+            engineering muscles within the business world. I aided internal
+            research on market segments by implenting Python scripts to automate
+            the flow of data collection. My research lead to key decisions in
+            product roadmap planning for Embedded SSDs. In the process, I
+            learned foundational skills such managing projects and understanding
+            the role of stakeholders in within those projects.
+          </Card>
+        </div>
+      </div>
+      <div
+        className="columns is-centered"
+        data-aos="fade-up"
+        data-aos-delay="75"
+      >
+        <div className="column is-three-quarters">
+          <Card
+            imgSrc={uopTower}
+            href="https://pacific.edu"
+            jobTitle="Bachelor of Science, Computer Science"
+            company="University of the Pacific"
+            length="2014-2018"
+            location="Stockton, CA"
+          >
+            UOP&apos;s computer science program helped me build the fundamentals
+            in becoming an engineer. Faculty push students to seek real world
+            experience through co-op, which I was forunate enough to partake in
+            with Intel. I was deeply engaged in community service through Alpha
+            Phi Omega, an international service organization. My experience at
+            UOP helped shape critical life skills, including empathy,
+            understanding, communication, and leadership.
+          </Card>
         </div>
       </div>
     </div>
