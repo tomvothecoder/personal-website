ff8a2bd78d3c5fe5aed17531bd3501d40228e793
tomvothecoder <tomvothecoder@gmail.com>
2020-04-18 11:32:41 -0700

    Add front-end react to begin port

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from "./views/Home"
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <NavBar/>
+      <Home/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
