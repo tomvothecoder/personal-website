7200cbda4350ce20363d8d901c67730cf33242b7
Tom Vo <tomvothecoder@gmail.com>
2020-11-11 16:11:10 -0800

    Update about description

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -12,24 +12,31 @@
               </div>
               <div class="column is-three-quarters">
                 <p class="job-desc">
-                  As a software engineer, I really enjoy the thought process
-                  involved in planning and executing new imaginative ideas.
-                  Aside from programming, my passions lie in fitness, hiking,
-                  photography, and eating the best burrito that a town has to
-                  offer. Catch me around the California central valley and bay
+                  As a software engineer, I enjoy the thought process involved
+                  in the entire lifecycle of ideas. From prototyping to the
+                  implementation phase, I strive to follow a pragamatic approach
+                  and use effective metholodogies to build robust tools. A wise
+                  programmer once said, "don't forget to test your code!"
+                </p>
+                <p class="job-desc">
+                  Aside from programming, my passions include powerlifting,
+                  running, hiking, watching MMA, and eating the best burrito
+                  that a town has to offer. Catch me around the California bay
                   area!
                 </p>
 
                 <div class="container has-text-centered">
-                  <h1 id="lets-connect">
+                  <h2 id="lets-connect">
                     Let's get in touch. LinkedIn or email works best.
+                  </h2>
+                  <h1 class="has-text-weight-bold	">
+                    Email: tomvothecoder@gmail.com
                   </h1>
-                  <h1 class="has-text-weight-bold	">Email: votom96@gmail.com</h1>
                   <div class="columns is-mobile is-multiline is-centered">
                     <div class="column is-narrow">
                       <p class="bd-notification is-primary">
                         <a
-                          href="https://www.linkedin.com/in/tomvo16/"
+                          href="https://www.linkedin.com/in/votom/"
                           id="linkedin-icon"
                           target="_blank"
                         >
@@ -40,7 +47,7 @@
                     <div class="column is-narrow">
                       <p class="bd-notification is-primary">
                         <a
-                          href="https://github.com/tvo25"
+                          href="https://github.com/tomvothecoder"
                           id="github-icon"
                           target="_blank"
                         >
@@ -68,7 +75,7 @@ export default {
 
 <style scoped>
 #lets-connect {
-  margin-top: 4%;
+  margin-top: 1rem;
   font-weight: 600;
 }
 </style>
