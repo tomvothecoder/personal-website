625acf2919dd60500ab67d35e100d3d174fe335a
tomvothecoder <tomvothecoder@gmail.com>
2021-05-22 12:40:59 -0700

    Add Vue router redirect - Remove unused script in package.json - Update vue.config.js publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  publicPath: '',
-  devServer: {
-    proxy: {
-      '^/api/': {
-        target: 'http://127.0.0.1:8000/api/',
-        ws: false
-      }
-    }
-  },
-  assetsDir: 'static'
+  publicPath: '/dist'
 };
